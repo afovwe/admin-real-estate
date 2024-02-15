aa9de3b48f512db762514f69a1737b0ffefe61aa
Fovwe <afovwe@gmail.com>
2024-02-15 06:48:49 +0100

    mongodb connectio created

--- api/index.js
+++ api/index.js
@@ -1,5 +1,20 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
+dotenv.config();
+
+
+
+mongoose.connect(process.env.MONGO)
+  .then(() => {
+    console.log('Connected to MongoDB!');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+  
 const app = express();
 const port = process.env.PORT || 7000; 
 app.listen(port, () => {
