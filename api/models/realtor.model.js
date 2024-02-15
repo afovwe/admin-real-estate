aff18d71c6ba19706ce78f69a6e6cd769bf61152
Fovwe <afovwe@gmail.com>
2024-02-15 21:59:40 +0100

    realtor model created

---
+++ api/models/realtor.model.js
@@ -0,0 +1,48 @@
+import mongoose from 'mongoose';
+
+const realtorSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },     
+    password: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+     realtorCid: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+      sponsorCid: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    avatar:{
+      type: String,
+      default: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
+    },
+    isVerified: {
+      type: Boolean,
+      default: false, // Initial status set to false
+    },
+  },
+  { timestamps: true }
+);
+
+const Realtor = mongoose.model('Realtor', realtorSchema);
+
+export default Realtor;
