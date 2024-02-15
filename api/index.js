71b9e0614f95f1de8fb05cb1e9c0374aac2b1d60
Fovwe <afovwe@gmail.com>
2024-02-15 04:57:46 +0100

    server created

---
+++ api/index.js
@@ -0,0 +1,7 @@
+import express from 'express';
+
+const app = express();
+const port = process.env.PORT || 7000; 
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}!!!`);
+});
