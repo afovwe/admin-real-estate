a42ab048f99f91b987d821e90b1e06ac3725427a
Fovwe <afovwe@gmail.com>
2024-02-21 02:16:18 +0100

    test apit route created

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import realtorRoute from './routes/realtor.route.js';
 
 dotenv.config();
 
@@ -8,7 +9,7 @@ dotenv.config();
 
 mongoose.connect(process.env.MONGO)
   .then(() => {
-    console.log('Connected to MongoDB!');
+    console.log('Connection to database is successfull!!');
   })
   .catch((err) => {
     console.log(err);
@@ -20,3 +21,5 @@ const port = process.env.PORT || 7000;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}!!!`);
 });
+
+app.use('/api/realtor', realtorRoute);
