231844e33cc559b4d9fa4dc310cc0bc06eeb0ec3
Fovwe <afovwe@gmail.com>
2024-02-23 03:25:38 +0100

    specific error middleware created

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -2,55 +2,49 @@ import Realtor from "../models/realtor.model.js";
 import { v4 as uuidv4 } from 'uuid';
 import bcryptjs from 'bcryptjs';
 
-export const signup = async (req, res) => {
+
+export const signup = async (req, res, next) => {
    const { username, email, password, phoneNumber, sponsorCid } = req.body;
 
-   // Check if any required field is missing or empty
-   if (!username || username === '') {
-      return res.status(400).json({ message: "Username is required" });
-   }
-   if (!email || email === '') {
-      return res.status(400).json({ message: "Email is required" });
-   }
-   if (!password || password === '') {
-      return res.status(400).json({ message: "Password is required" });
-   }
-   if (!phoneNumber || phoneNumber === '') {
-      return res.status(400).json({ message: "Phone number is required" });
-   }
-   if (!sponsorCid || sponsorCid === '') {
-      return res.status(400).json({ message: "Sponsor CID is required" });
-   }
-
-   // Generate unique realtorCid
-   const realtorCid = generateUniqueCid();
-
-   const hashedPassword = bcryptjs.hashSync(password, 10);
-
-   const newRealtor = new Realtor({
-      username,
-      email,
-      password: hashedPassword,
-      phoneNumber,
-      sponsorCid,
-      realtorCid, // Assign generated realtorCid
-      isVerified: false // Set isVerified to false
-   });
-
-   
    try {
+      // Check if any required field is missing or empty
+      if (!username || username === '') {
+         throw { statusCode: 400, message: "Username is required" };
+      }
+      if (!email || email === '') {
+         throw { statusCode: 400, message: "Email is required" };
+      }
+      if (!password || password === '') {
+         throw { statusCode: 400, message: "Password is required" };
+      }
+      if (!phoneNumber || phoneNumber === '') {
+         throw { statusCode: 400, message: "Phone number is required" };
+      }
+      if (!sponsorCid || sponsorCid === '') {
+         throw { statusCode: 400, message: "Sponsor CID is required" };
+      }
+
+      // Generate unique realtorCid
+      const realtorCid = generateUniqueCid();
+
+      const hashedPassword = bcryptjs.hashSync(password, 10);
+
+      const newRealtor = new Realtor({
+         username,
+         email,
+         password: hashedPassword,
+         phoneNumber,
+         sponsorCid,
+         realtorCid, // Assign generated realtorCid
+         isVerified: false // Set isVerified to false
+      });
+
       // Save the new realtor to the database
       await newRealtor.save();
       res.json('Sign up successful!!!');
    } catch (error) {
-      // Handle duplicate key error
-      if (error.name === 'MongoServerError' && error.code === 11000) {
-         const errorMessage = getDuplicateKeyErrorMessage(error.keyValue);
-         return res.status(400).json({ message: errorMessage });
-      }
-      // Handle other errors
-      console.error("Error while signing up:", error);
-      res.status(500).json({ message: "Failed to sign up" });
+      // Pass the error to the error-handling middleware
+      next(error);
    }
 }
 
--- api/index.js
+++ api/index.js
@@ -26,3 +26,24 @@ app.listen(port, () => {
 
 app.use('/api/realtor', realtorRoutes);
 app.use('/api/auth', authRoutes);
+
+app.use((err, req, res, next) => {
+    if (err && err.message && err.message.includes('duplicate key error collection')) {
+        if (err.message.includes('username')) {
+            return res.status(400).json({ success: false,  statusCode: 400, message: "A user with this username already exists" });
+        } else if (err.message.includes('email')) {
+            return res.status(400).json({ success: false,  statusCode: 400, message: "A user with this email already exists" });
+        } else if (err.message.includes('phoneNumber')) {
+            return res.status(400).json({ success: false,  statusCode: 400,message: "A user with this phone number already exists" });
+        }
+    }
+
+    // If the error is not a duplicate key error or cannot be handled, return a generic error
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    res.status(statusCode).json ({
+        success: false,
+        statusCode,
+        message
+    });
+});
